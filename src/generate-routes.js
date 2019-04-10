eacc6b683baeb93d2cdaad60d0a1829450a43f70
Chris Calo <chris.calo@gmail.com>
2019-04-10 11:33:54 -0400

    Simplify route generation script

--- src/generate-routes.js
+++ src/generate-routes.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fse = require("fs-extra");
 const path = require("path");
 const { generateRoutes } = require("vue-route-generator");
 
@@ -6,21 +6,4 @@ const code = generateRoutes({
   pages: path.join(__dirname, "./pages"),
 });
 
-writeFileSyncRecursive(path.join(__dirname, "./router/routes.js"), code);
-
-// from https://gist.github.com/drodsou/de2ba6291aea67ffc5bc4b52d8c32abd
-// like writeFileSync but creates all folder paths if not exist
-// ----------------------------------------------------------
-function writeFileSyncRecursive(filename, content, charset) {
-  // create folder path if not exists
-  filename
-    .split("/")
-    .slice(0, -1)
-    .reduce((last, folder) => {
-      let folderPath = last ? last + "/" + folder : folder;
-      if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath);
-      return folderPath;
-    });
-  
-  fs.writeFileSync(filename, content, charset);
-}
+fse.outputFileSync(path.join(__dirname, "./router/routes.js"), code);
