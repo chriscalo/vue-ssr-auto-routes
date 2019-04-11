04ce9fe7b429fe2a57ae098cea7079c625db383a
Chris Calo <chris.calo@gmail.com>
2019-04-11 18:31:14 -0400

    Move route generator

--- src/router/generate.js
+++ src/router/generate.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { generateRoutes } = require("vue-route-generator");
 
 const code = generateRoutes({
-  pages: path.join(__dirname, "./pages"),
+  pages: path.join(__dirname, "../pages"),
 });
 
-fse.outputFileSync(path.join(__dirname, "./router/routes.js"), code);
+fse.outputFileSync(path.join(__dirname, "./routes.js"), code);
